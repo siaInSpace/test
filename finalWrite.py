2af9263a93f991e2c8ddaf33806c53f1d7f6b42a
siaa20 <37308360+siaa20@users.noreply.github.com>
2018-03-21 23:08:32 +0000

    added multithread stuff

--- finalWrite.py
+++ finalWrite.py
@@ -1,5 +1,8 @@
+import time
+from multithreading import threadpool as pool
 import Adafruit_BMP.BMP085 as BMP085
 from mpu6050 import mpu6050
+
 s185 = BMP085.BMP085()
 s6050 = mpu6050(0x68)
 units = {0: 'x', 1: 'y', 2: 'z'}
@@ -11,15 +14,32 @@ def fileWrite(info):
 def fileWriteArray(info):
 	for i in range(0, 3):
 		filWrite(info[units[i]])
-def fileWriteNewLine(info):
-	info = ("{0:3.3f}".format(info))
-	f.write(info)
-	f.write('\n')
-
+startTime = time.time()
 while True:
+	filWrite(time.time()-startTime)
 	fileWriteArray(s6050.get_gyro_data())
 	fileWriteArray(s6050.get_accel_data())
 	fileWrite(s185.read_temperature())
 	fileWrite(s185.read_pressure())
 	fileWrite(s185.read_altitude())
 	f.write('/n')
+	
+	
+def main():
+	p = pool(20)
+	q = queue()
+	p.apply(readGPS, q)
+	p.apply(readI2c, q)
+	
+	while true:
+		data = new [] ''' check new object syntax'''
+		for range(0, 500):
+			currData = makeCurrData()
+			data.append(currData)
+			
+		p.apply(dataToFile, data)	
+		
+		
+	
+if __name__ is "__main__":
+	main()
