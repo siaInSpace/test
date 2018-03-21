9e6896f4fd8e934b8617060d63417f82eca01505
siaa20 <37308360+siaa20@users.noreply.github.com>
2018-03-21 21:32:59 +0000

    write from 180 and 5060 sennsors to a file

---
+++ finalWrite.py
@@ -0,0 +1,25 @@
+import Adafruit_BMP.BMP085 as BMP085
+s185 = BMP085.BMP085()
+from mpu6050 import mpu6050
+s6050 = mpu6050(0x68)
+units = {0: 'x', 1: 'y', 2: 'z'}
+f = open('OutputFile', 'w')
+def fileWrite(info):
+	info = ("{0:3.3f}".format(info))
+	f.write(info)
+	f.write(":")
+def fileWriteArray(info):
+	for i in range(0, 3):
+		filWrite(info[units[i]])
+def fileWriteNewLine(info):
+	info = ("{0:3.3f}".format(info))
+	f.write(info)
+	f.write('\n')
+
+while True:
+	fileWriteArray(s6050.get_gyro_data())
+	fileWriteArray(s6050.get_accel_data())
+	fileWrite(s185.read_temperature())
+	fileWrite(s185.read_pressure())
+	fileWrite(s185.read_altitude())
+	f.write('/n')
