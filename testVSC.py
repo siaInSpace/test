b4a6311d499a073f1cf8f6bdd878b18061237f05
siaa20 <37308360+siaa20@users.noreply.github.com>
2018-03-28 02:23:45 +0200

    added a for loop to print sqaures from 0 to 3 with incremnts of 0.1

--- testVSC.py
+++ testVSC.py
@@ -1,2 +1,6 @@
 print("helloWorldTest!")
 print("This is writen in visual studio code, and it looks good for now!")
+print("now we are testing pip8's functions on this code")
+print("it seems to work fine!")
+for i in range(0, 3 , 0.1):
+        print(i, i*i)
