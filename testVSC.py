e69fbaf5bcfb55bf20919169748a2d1eb24ede42
siaa20 <37308360+siaa20@users.noreply.github.com>
2018-03-28 02:29:56 +0200

    fixed for loop to use integers for range()

--- testVSC.py
+++ testVSC.py
@@ -2,5 +2,5 @@ print("helloWorldTest!")
 print("This is writen in visual studio code, and it looks good for now!")
 print("now we are testing pip8's functions on this code")
 print("it seems to work fine!")
-for i in range(0, 3 , 0.1):
-        print(i, i*i)
+for i in range(0, 10, 2):
+    print(i, i*i)
